872caa6b723aeef290b43ca9ab9aeeb1c75b9608
JC <jcsmileyjr@gmail.com>
2017-06-13 21:16:16 -0500

    Build inital views, libaries, learn new navigation
    1. Build index.html with UI-ROUTER for navigation
    - Research, learned, and utilize the UI-ROUTER libary.
    - Build initial index.html with new navigaiton and ui-view to rout
    internal pages
    - Build mock logIn, input, and stats views.
    - Build app.js file to initilize angular module and config function for
    routing
    - Organize views into view folder
    - Organize header files into libs folder

---
+++ app.js
@@ -0,0 +1,25 @@
+var myApp = angular.module('UvT', ['ui.router']);
+
+myApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/logIn');
+
+    $stateProvider
+        .state('logIn', {
+            url:'/logIn',
+            templateUrl: 'views/logIn.html'
+        })
+        .state('input', {
+            url:'/input',
+            templateUrl: 'views/input.html'
+        })
+        .state('stats', {
+            url: '/stats',
+            templateUrl: 'views/stats.html'
+        })
+     
+    
+}]);
+
+myApp.controller('mainController', function($scope){
+});
