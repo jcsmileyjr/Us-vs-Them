39693a59312a2243808002010f732395dd1ae51f
JC <jcsmileyjr@gmail.com>
2017-06-19 20:52:52 -0500

    Add getPlayerTeamRank to Players Service
    1. Create a method in the Players service that gets all the team members
    of the current player from the players array to find the current rank of
    the current player compared to other team members.
    - use a for loop in the players array to place all players objects
    matching the teamName of the currentPlayer object into a playersOnTeam
    array of objects.
    - decending sort the playersOnTeam array using a array.sort compare
    method.
    - use a for loop to search the sorted playersOnTeam array for the
    jerseyNumber of the currentPlayer object. If found, use the
    array.indexOf to get the current index plus 1 to get the rank.

--- playersService.js
+++ playersService.js
@@ -1,7 +1,7 @@
 
 myApp.factory('Players', function(){
 
-    var players = [{"jerseyNumber":"1", "teamName": "Globe Trotters", "playerTotalPoint":"200", "playerName":"Bob Dylan"}, {"jerseyNumber":"2", "teamName": "Almost Retire", "playerTotalPoint":"250", "playerName":"Dole White"}, {"jerseyNumber":"3", "teamName": "Bad Boys and Girls", "playerTotalPoint":"85", "playerName":"Bertha Sings"}, {"jerseyNumber":"4", "teamName": "Cookie Cutters", "playerTotalPoint":"280", "playerName":"Mary Beth Loy"}, {"jerseyNumber":"5", "teamName": "Rolling Stones", "playerTotalPoint":"390", "playerName":"Nate Shorts"}, {"jerseyNumber":"6", "teamName": "Globe Trotters", "playerTotalPoint":"220", "playerName":"Pink Panther"}];
+    var players = [{"jerseyNumber":"7", "teamName": "Globe Trotters", "playerTotalPoint":"240", "playerName":"Nick Cage"}, {"jerseyNumber":"1", "teamName": "Globe Trotters", "playerTotalPoint":"200", "playerName":"Bob Dylan"}, {"jerseyNumber":"2", "teamName": "Almost Retire", "playerTotalPoint":"250", "playerName":"Dole White"}, {"jerseyNumber":"3", "teamName": "Bad Boys and Girls", "playerTotalPoint":"85", "playerName":"Bertha Sings"}, {"jerseyNumber":"4", "teamName": "Cookie Cutters", "playerTotalPoint":"280", "playerName":"Mary Beth Loy"}, {"jerseyNumber":"5", "teamName": "Rolling Stones", "playerTotalPoint":"390", "playerName":"Nate Shorts"}, {"jerseyNumber":"6", "teamName": "Globe Trotters", "playerTotalPoint":"220", "playerName":"Pink Panther"}];
     
     var currentPlayer = {};
     
@@ -29,7 +29,28 @@ myApp.factory('Players', function(){
             return currentPlayer;
         },/*End of getCurrentPlayer function*/
         
+        getPlayerTeamRank: function(){
+            var playersOnTeam = [];
+            for(var i=0;i<players.length;i++){
+                if(players[i].teamName == currentPlayer.teamName){
+                    playersOnTeam.push(players[i]);
+                }/*End of if statement*/
+            }/*End of for loop*/
 
+            /*sorting the playersOnTeam array from highest to lowest based on the playerTotalPoint property*/
+            playersOnTeam.sort(function(a, b){
+                return b.playerTotalPoint - a.playerTotalPoint;
+            });
+
+            for(var j=0;j<playersOnTeam.length;j++){
+                console.log(playersOnTeam[j].jerseyNumber);
+                console.log(currentPlayer.jerseyNumber);
+                if(currentPlayer.jerseyNumber == playersOnTeam[j].jerseyNumber){
+                    return playersOnTeam.indexOf(playersOnTeam[j]) + 1;
+            }/*End of if statement*/
+            }/*End of j's for loop*/                  
+            
+        }/*End of getPlayerTeamRank method*/
     }/*End of Players Factory object return*/
     
     
